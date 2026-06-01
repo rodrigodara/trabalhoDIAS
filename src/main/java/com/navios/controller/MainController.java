6075df33d90fe6996cef60bcb457b2ef7b3ef8a0
rfernandesit07 <1251695@isep.ipp.pt>
2026-06-01 20:33:07 +0100

    Fix da Sidebar

--- src/main/java/com/navios/controller/MainController.java
+++ src/main/java/com/navios/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class MainController {
 
@@ -22,6 +23,24 @@ public class MainController {
 
     @FXML
     private AnchorPane mainContent;
+
+    @FXML
+    private VBox navButtons;
+
+    @FXML
+    private VBox sidebar;
+
+    private boolean expanded = true;
+    private static final double SIDEBAR_EXPANDED = 160.0;
+    private static final double SIDEBAR_COLLAPSED = 50.0;
+
+    @FXML
+    private void handleToggleMenu() {
+        expanded = !expanded;
+        navButtons.setVisible(expanded);
+        navButtons.setManaged(expanded);
+        sidebar.setPrefWidth(expanded ? SIDEBAR_EXPANDED : SIDEBAR_COLLAPSED);
+    }
 
     @FXML
     private void handleNavios() {
@@ -47,6 +66,10 @@ public class MainController {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
             Parent view = loader.load();
+            AnchorPane.setTopAnchor(view, 0.0);
+            AnchorPane.setBottomAnchor(view, 0.0);
+            AnchorPane.setLeftAnchor(view, 0.0);
+            AnchorPane.setRightAnchor(view, 0.0);
             mainContent.getChildren().clear();
             mainContent.getChildren().add(view);
         } catch (Exception e) {
